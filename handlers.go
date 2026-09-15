7112a7363a77722825d8c8f866cf70f165ab9024
getdhed <nazarzhebrun@gmail.com>
2026-09-15 15:22:02 +0300

    add auth middleware

--- handlers.go
+++ handlers.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func myHandler(w http.ResponseWriter, r *http.Request) {
@@ -33,6 +36,45 @@ func errorHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func loginHandler(w http.ResponseWriter, r *http.Request) {
+func confirmEmail(email string) bool {
+	return email != ""
+}
 
+func confirmPassword(password string) bool {
+	return password != ""
+}
+
+func (s *Server) loginHandler(w http.ResponseWriter, r *http.Request) {
+	var req loginRequest
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		if err := writeError(w, http.StatusBadRequest, "bad request", "bad request"); err != nil {
+			fmt.Println("Произошла ошибка", err)
+		}
+		return
+
+	}
+
+	if !confirmPassword(req.Password) || !confirmEmail(req.Email) {
+		if err := writeError(w, http.StatusBadRequest, "bad request", "bad request"); err != nil {
+			fmt.Println("Произошла ошибка", err)
+		}
+		return
+	}
+	user, isExist := s.userStore.FindByEmail(req.Email)
+	if !isExist {
+		if err := writeError(w, http.StatusUnauthorized, "Unauthorized", "Unauthorized"); err != nil {
+			fmt.Println("Произошла ошибка", err)
+		}
+		return
+	}
+	if err := bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(req.Password)); err != nil {
+		if err := writeError(w, http.StatusUnauthorized, "Unauthorized", "Unauthorized"); err != nil {
+			fmt.Println("Произошла ошибка", err)
+		}
+		return
+	}
+	if err := writeJSON(w, 200, user); err != nil {
+		fmt.Println("Произошла ошибка", err)
+	}
 }
--- models.go
+++ models.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type responseWriter struct {
@@ -20,6 +22,7 @@ type errorResponse struct {
 type Server struct {
 	httpServer *http.Server
 	mux        *http.ServeMux
+	userStore  *UserStore
 }
 
 type User struct {
@@ -37,11 +40,46 @@ type loginResponse struct {
 	TokenType   string `json:"token_type"`
 	ExpiresIn   int    `json:"expires_in"`
 }
+type UserStore struct {
+	users map[string]User
+}
+
+func NewUserStore() (*UserStore, error) {
+	us := UserStore{
+		users: make(map[string]User),
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte("qwerty123"), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	testUser := User{
+		ID:           1,
+		Email:        "testmail",
+		PasswordHash: string(hash),
+	}
+
+	us.users["testmail"] = testUser
+	return &us, nil
+}
+
+func (us *UserStore) FindByEmail(email string) (User, bool) {
+	user, ok := us.users[email]
+	return user, ok
+}
 
 func NewServer(addr string) *Server {
 	mux := http.NewServeMux()
+
+	userStore, err := NewUserStore()
+	if err != nil {
+		return nil
+	}
+
 	s := &Server{
-		mux: mux,
+		mux:       mux,
+		userStore: userStore,
 	}
 	s.httpServer = &http.Server{
 		Addr:              addr,
